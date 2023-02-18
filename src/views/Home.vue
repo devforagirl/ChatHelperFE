d685f8dd001e673eb68a7988f3db79b7d48bb553
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-18 13:02:57 +0000

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,64 @@
 <template>
-  <div class="centered-input">
+  <div class="HomeBigBox">
+    <div v-if="!user_info">
     <v-container fluid>
       <v-row justify="center">
         <v-col cols="12" sm="8" md="4">
-          <v-text-field v-model="inputValue" label="Open Sesame!" />
-        </v-col>
-      </v-row>
-      <div>{{ decode }}</div>
-    </v-container>
-
-    <v-container>
-      <v-row>
-        <v-col cols="12">
           <v-text-field
-            v-model="videoIdAddress"
-            :rules="[rules.required, rules.min]"
+            v-model="inputValue"
+            label="Open Sesame!"
             filled
             clear-icon="mdi-close-circle"
             clearable
-            label="Video Address"
             type="text"
-            @click:append-outer="sendVid"
-            @click:clear="clearMessage"
+            @input="handleInput"
           />
         </v-col>
       </v-row>
     </v-container>
   </div>
+
+    <div
+    v-else
+      class="welcomebackBox"
+    >
+      <span> Welcome back, </span>
+      <b> {{ user_info.name }}</b><br>
+      <router-link to="dashboard"> Go to Dashboard </router-link>
+    </div>
+
+  </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default {
   data() {
     return {
-      inputValue: "",
-    };
+      inputValue: ''
+    }
+  },
+  computed: {
+    ...mapState(['user_info'])
   },
   methods: {
-    clearMessage() {
-      this.$store.commit("set_videoIdAddress", "")
-    },
-    sendVid() {
-      this.$store.dispatch("action_updateVideoId", this.videoIdAddress)
+    handleInput() {
+      console.log('inputValue changed to', this.inputValue)
+      if (this.inputValue === process.env.VUE_APP_SESAME) {
+        this.$router.push('/authenticate')
+      }
     }
   }
-  // computed: {
-  //   decode() {
-  //     if (this.inputValue === process.env.VUE_APP_SESAME) {
-  //       // console.log('inputValue->', this.inputValue)
-  //       this.$router.push('authenticate')
-  //     }
-  //     return '233'
-  //   }
-  // }
 }
 </script>
 
 <style>
-.centered-input {
+.HomeBigBox {
+  background-color: #222;
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 100vh;
+  height: 100vh
 }
 </style>
