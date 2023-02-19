67f389998e66cbaacf0baa54efd6d2d3b3e9c8aa
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-19 12:52:37 +0000

    create dist

---
+++ dist/precache-manifest.99144e1dac4e5e9e1700065ea4555858.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c191b2ced055107ec13",
+    "url": "/css/app.3cc8c3b5.css"
+  },
+  {
+    "revision": "a0dfe943da99aede5178",
+    "url": "/css/chunk-vendors.d12c6b29.css"
+  },
+  {
+    "revision": "174898fc53a56919a5cc2708856940d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c191b2ced055107ec13",
+    "url": "/js/app.808336c6.js"
+  },
+  {
+    "revision": "a0dfe943da99aede5178",
+    "url": "/js/chunk-vendors.94180916.js"
+  },
+  {
+    "revision": "a583df2bc3aec45c9ae7ef6341e36837",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
