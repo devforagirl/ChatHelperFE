8c38ce10bcd8d5a7656c477413c2461eccfae9a7
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-20 18:37:18 +0800

    set a new address to /socket.io

--- vue.config.js
+++ vue.config.js
@@ -6,7 +6,8 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       '/socket.io': {
-        target: 'http://127.0.0.1:5000',
+        // target: 'http://127.0.0.1:5000',
+        target: 'https://chat-helper-be-2.vercel.app',
 
         changeOrigin: true
       }
