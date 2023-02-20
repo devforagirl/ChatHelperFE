3d5aebb43f6a6f82e78ae72b07aa12adc8920d42
devforagirl <48156801+devforagirl@users.noreply.github.com>
2023-02-20 18:53:17 +0800

    本地开发时使用的

--- vue.config.js
+++ vue.config.js
@@ -5,9 +5,10 @@ module.exports = {
   devServer: {
     disableHostCheck: true,
     proxy: {
+      // 本地开发时使用的
       // '/socket.io': {
       //   // target: 'http://127.0.0.1:5000',
-      //   // target: 'https://chat-helper-be-2.vercel.app', 好像没做用了
+      //   // target: 'https://chat-helper-be-2.vercel.app', 
       //   target: 'https://nba.com',
 
       //   changeOrigin: true
